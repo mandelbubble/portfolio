9e1085fe1d61a4b45adb7b8c639bb4d44ce24a5c
mandelbubble <alexperesson.dev+git@gmail.com>
2023-09-10 19:01:18 +0200

    Favicon and General metadatas

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,8 +6,30 @@ import { vt323 } from './lib/fonts'
 import '@/app/styles/globals.scss'
 
 export const metadata = {
-  title: 'Alex Peresson Portfolio',
-  description: 'Welcome to my portfolio',
+  siteName: 'Alex Peresson porfolio',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['Next.js', 'React', 'JavaScript', 'Three.js', 'WebGL'],
+  colorScheme: 'dark',
+  creator: 'Alex Peresson',
+  authors: [{ name : 'Alex'}],
+  title: {
+    default : 'Alex Peresson',
+    template : '%s | Alex Peresson'
+  },
+  description: 'Software engineer portfolio',
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    }
+  }
 }
 
 export default function RootLayout({ children }) {
